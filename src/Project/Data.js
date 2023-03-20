5c0889be77af26d80d9ab793c768e0950004a6f9
fares <fares12yaseer45@gmail.com>
2023-03-20 21:56:14 +0200

    add main files and create the container

---
+++ src/Project/Data.js
@@ -0,0 +1,60 @@
+import AngelaGray from './images/avatar-angela-gray.webp'
+import AnaKim from './images/avatar-anna-kim.webp'
+import JacobThopmson from './images/avatar-jacob-thompson.webp'
+import KemberlySmith from './images/avatar-kimberly-smith.webp'
+import MarkWebber from './images/avatar-mark-webber.webp'
+import NathanPetrson from './images/avatar-nathan-peterson.webp'
+import RizkyHasanuddin from './images/avatar-rizky-hasanuddin.webp'
+
+export const data = [
+    {
+        image : MarkWebber,
+        name : 'Mark Webber',
+        message : 'reacted to your recent post',
+        time : '1M Ago',
+        reacted : 'my first tournmint today'
+    },
+    {
+        image : AngelaGray,
+        name : 'Angela Gray',
+        message : 'followed you',
+        time : '5M Ago',
+        reacted : 'Chess Club'
+    },
+    {
+        image : JacobThopmson,
+        name : 'Jacob Thopmson',
+        message : 'has joined your group',
+        time : '1 Day Ago',
+        reacted : '',
+        content : ''
+    },
+    {
+        image : RizkyHasanuddin,
+        name : 'Rizky Hasanuddin',
+        message : 'sent you a private message',
+        time : '5 Days Ago',
+        reacted : ''
+    },
+    {
+        image : KemberlySmith,
+        name : 'Kemberly Smith',
+        message : 'commented on your pictuer',
+        time : '1 Week Ago',
+        reacted : ''
+    },
+    {
+        image : NathanPetrson,
+        name : 'Nathan Petrson',
+        message : 'reacted to your recent post',
+        time : '2 Week Ago',
+        reacted : ''
+    },
+    {
+        image : AnaKim,
+        name : 'Ana Kim',
+        message : 'left the group',
+        time : '2 Week Ago',
+        reacted : 'Chess Club'
+    },
+]
---
+++ src/Project/Project.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Style.css'
+
+const Project = () => {
+    return (
+        <div className='project'>
+            <div className="container"></div>
+        </div>
+    )
+}
+
+export default Project
